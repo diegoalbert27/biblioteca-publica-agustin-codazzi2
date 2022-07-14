f83bbe4797842581ea9032a14cbcad03d67ad99a
dHinagas <hinagasrodriguez@gmail.com>
2022-07-14 10:51:45 -0400

    removed code small and added tatables

--- views/prestamoView/partialsPrestamo/prestamoView.php
+++ views/prestamoView/partialsPrestamo/prestamoView.php
@@ -21,29 +21,6 @@ if (isset($_REQUEST['r'])) {
 
 $result = $resultPres;
 
-// define('NUM_ITEMS_BY_PAGE', 10);
-
-// $num_total_rows = count($resultPres);
-
-// if ($num_total_rows > 0) {
-//     $page = false;
-
-//     //examino la pagina a mostrar y el inicio del registro a mostrar
-//     if (isset($_GET["page"])) {
-//         $page = $_GET["page"];
-//     }
-
-//     if (!$page) {
-//         $start = 0;
-//         $page = 1;
-//     } else {
-//         $start = ($page - 1) * NUM_ITEMS_BY_PAGE;
-//     }
-//     //calculo el total de paginas
-//     $total_pages = ceil($num_total_rows / NUM_ITEMS_BY_PAGE);
-
-//     $result = array_slice($resultPres, $start, NUM_ITEMS_BY_PAGE);
-
 ?>
 
 <div class="container">
@@ -61,7 +38,7 @@ $result = $resultPres;
     </div>
 
     <div class="p-3 mb-3">
-      <table id="table-prestamo" class="table">
+      <table id="table-prestamo" class="table lazy__table">
         <thead>
           <tr>
             <th scope="col">Número de carnet</th>
@@ -107,40 +84,5 @@ $result = $resultPres;
 
     </div>
 
-    <!-- <nav aria-label="Page navigation example">
-      <ul class="pagination justify-content-end my-6">
-        <?php
-        if ($total_pages > 1) {
-          if ($page != 1) {
-        ?>
-            <li class="page-item">
-              <a class="page-link" href="index.php?controller=prestamo&page=<?php echo $page - 1 ?>">
-                <span aria-hidden="true">Anterior</span>
-              </a>
-            </li>
-          <?php
-          }
-          for ($i = 1; $i <= $total_pages; $i++) {
-            if ($page == $i) {
-              echo '<li class="page-item active"><a class="page-link" href="#">' . $page . '</a></li>';
-            } else {
-              echo '<li class="page-item"><a class="page-link" href="index.php?controller=prestamo&page=' . $i . '">' . $i . '</a></li>';
-            }
-          }
-          if ($page != $total_pages) {
-          ?>
-            <li class="page-item">
-              <a class="page-link" href="index.php?controller=prestamo&page=<?php echo $page + 1 ?>">
-                <span aria-hidden="true">Siguiente</span>
-              </a>
-            </li>
-        <?php
-          }
-        }
-
-        ?>
-      </ul>
-
-    </nav> -->
   </section>
 </div>
